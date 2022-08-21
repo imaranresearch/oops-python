d7eb1120e3c400305c5dbcb05843f601c72ceed0
Mohammad Imran <imaranresearch@gmail.com>
2022-08-21 08:43:18 +0100

    Solved exercises uptop Chapter 2

---
+++ definition_start.py
@@ -0,0 +1,13 @@
+# TODO: Create a basic class
+class Book():
+    def __init__(self, title):
+        self.title = title
+
+
+# TODO: create instance of the class
+b1 = Book("Brave new World")
+b2 = Book("War and Peace")
+
+# TODO: print the class and property 
+print(b1)
+print(b1.title)
